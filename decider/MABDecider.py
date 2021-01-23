ddcb3f9119578106ee4410c6a7fb312aa1f75cae
Shreshth Tuli <shreshthtuli@gmail.com>
2021-01-23 18:08:46 +0530

    Update MABDecider.py

--- decider/MABDecider.py
+++ decider/MABDecider.py
@@ -66,7 +66,7 @@ class MABDecider(SplitDecision):
 				intervals = dict_['destroyAt'] - dict_['createAt']
 				sla = dict_['sla']
 				sla_reward = 1 if intervals <= sla else 0
-				acc_reward = dict_['result'][0]/dict_['result'][1]
+				acc_reward = min(0, (dict_['result'][0]/dict_['result'][1] - 0.85)) * 10
 				reward = Coeff_SLA * sla_reward + Coeff_Acc * acc_reward
 				rewards.append(reward)
 				low = sla < self.average_layer_intervals[workflow]
